e2530dd8ea8f992a97130ac76eeae83f297094c6
Marika Bergman <marika.bergman@gmail.com>
2023-09-23 08:48:30 +0000

    #9 add random terraform provider

--- main.tf
+++ main.tf
@@ -1,1 +1,23 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
+
+resource "random_string" "bucket_name" {
+  length           = 16
+  special          = false
+  override_special = "/@£$"
+}
+
+output "random_bucket_name_result" {
+    value = random_string.bucket_name.result
+}
 
