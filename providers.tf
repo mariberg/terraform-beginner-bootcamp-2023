b70538de752e9b568bcf5eaf010fcaed6451bdce
Marika Bergman <75071780+mariberg@users.noreply.github.com>
2023-09-26 12:18:57 +0100

    #21 change structure of root module and add tagging to bucket (#23)

---
+++ providers.tf
@@ -0,0 +1,24 @@
+terraform {
+  #cloud {
+  #  organization = "Marika"
+
+ #   workspaces {
+ #     name = "terra-house-1"
+ #   }
+ # }
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.16.2"
+    }
+  }
+}
+
+provider "aws" {}
+provider "random" {
+    # Configuration options
+}
