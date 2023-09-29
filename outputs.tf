cc0817bcfb07b15951320e6acaf12a0b8279d794
Marika Bergman <marika.bergman@gmail.com>
2023-09-29 12:34:00 +0000

    #28 enable static website hosting, upload index and error files via Terraform

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "bucket_name" {
     description = "Bucket name for our static website hosting"
     value = module.terrahouse_aws.bucket_name
 }
+
+output "S3_website_endpoint" {
+  description = "S3 static website hosting endpoint"
+  value = module.terrahouse_aws.website_endpoint
+}
