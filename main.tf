c79f2d279001c3d7a374c14a30961fb994990203
Marika Bergman <75071780+mariberg@users.noreply.github.com>
2023-10-12 13:20:13 +0100

    #47 test our terra home on terratowns test town (#48)

--- main.tf
+++ main.tf
@@ -8,20 +8,20 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = var.terratowns_endpoint
+  user_uuid = var.teacherseat_user_uuid
+  token= var.terratowns_access_token
 }
 
-#module "terrahouse_aws" {
-#  source = "./modules/terrahouse_aws"
-#  user_uuid = var.user_uuid
-#  bucket_name = var.bucket_name
-#  index_html_filepath = var.index_html_filepath
-#  error_html_filepath = var.error_html_filepath
-#  public_path = var.public_path
-#  content_version = var.content_version
-#}
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.teacherseat_user_uuid
+  bucket_name = var.bucket_name
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
+  public_path = var.public_path
+  content_version = var.content_version
+}
 
 resource "terratowns_home" "home" {
   name = "Uno Card Game in 2023"
@@ -30,7 +30,8 @@ resource "terratowns_home" "home" {
   With just a deck of colorful cards and a few straightforward rules, players embark
   on a thrilling journey of strategy and chance.
   DESCRIPTION 
-  domain_name = "3fdq3gz.cloudfront.net"
-  town = "gamers grotto"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  #domain_name = "3fee3gg.cloudfront.net"
+  town = "missingo"
   content_version = 1
 }
--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-#output "bucket_name" {
-#    description = "Bucket name for our static website hosting"
-#    value = module.terrahouse_aws.bucket_name
-#}
-#
-#output "S3_website_endpoint" {
-#  description = "S3 static website hosting endpoint"
-#  value = module.terrahouse_aws.website_endpoint
-#}
+output "bucket_name" {
+    description = "Bucket name for our static website hosting"
+    value = module.terrahouse_aws.bucket_name
+}
 
-#output "cloudfront_url" {
-#  description = "The CloudFront distribution domain name"
-#  value = module.terrahouse_aws.cloudfront_url
-#}
+output "S3_website_endpoint" {
+  description = "S3 static website hosting endpoint"
+  value = module.terrahouse_aws.website_endpoint
+}
+
+output "cloudfront_url" {
+  description = "The CloudFront distribution domain name"
+  value = module.terrahouse_aws.cloudfront_url
+}
