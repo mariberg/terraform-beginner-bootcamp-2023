ac39d7e0e8fa897c3876fe3a0daaeaea2f551e94
Marika Bergman <marika.bergman@gmail.com>
2023-10-12 12:18:49 +0000

    #47 test our terra home on terratowns test town

--- variables.tf
+++ variables.tf
@@ -1,6 +1,11 @@
-variable "user_uuid" {
+variable "teacherseat_user_uuid" {
   type = string
 }
+
+variable "terratowns_access_token" {
+  type = string
+}
+
 
 variable "bucket_name" {
   type = string
@@ -21,5 +26,10 @@ variable "content_version" {
 
 variable "public_path" {
   description = "Path to public folder"
+  type = string 
+}
+
+variable "terratowns_endpoint" {
+  description = "Terratowns cloud endpoint"
   type = string 
 }
