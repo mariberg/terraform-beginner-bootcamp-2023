598b342f91a99fbc0cfe625fe043e0099bb4c299
Marika Bergman <75071780+mariberg@users.noreply.github.com>
2023-10-14 00:11:23 +0100

    #53 modify html file and remove one of the homes (#54)

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "bucket_name" {
     description = "Bucket name for our static website hosting"
-    value = module.home_uno_hosting.bucket_name
+    value = module.home_licorice_hosting.bucket_name
 }
 
 output "S3_website_endpoint" {
   description = "S3 static website hosting endpoint"
-  value = module.home_uno_hosting.website_endpoint
+  value = module.home_licorice_hosting.website_endpoint
 }
 
 output "cloudfront_url" {
   description = "The CloudFront distribution domain name"
-  value = module.home_uno_hosting.domain_name
+  value = module.home_licorice_hosting.domain_name
 }
